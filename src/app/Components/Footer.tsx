c63b71e47262168fe2cd162954d1e1e55e70c13d
José Cláudio <jvalentino9.2005@gmail.com>
2024-12-16 16:13:04 +0000

    a

--- src/app/Components/Footer.tsx
+++
@@ -1,9 +0,0 @@
-const Footer = () => {
-    return(
-        <div>
-          <h1>League of Heroes - Copyright © 2024 by José Cláudio</h1>  
-        </div>  
-    )
-}
-
-export default Footer;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,20 @@
-import Image from "next/image";
+"use client"
 import styles from "./page.module.css";
 import Header from './Components/Header';
 import Footer from './Components/Footer';
-import Content from './Components/Content';
+import Content from './Components/Content.jsx';
+import './globals.css'
+import React, {useState} from "react";
+
 export default function Home() {
+  const [my_name] = useState<String>("José Cláudio")
+  const [project_name] = useState<String>("League of Heroes")
+
   return (
-   <div>
-    <Header ></Header>
-    <Content></Content>
-    <Footer></Footer> 
+   <div className={styles.page}>
+      <Header my_name={my_name} project_name={project_name}></Header>
+      <Content></Content>
+       <Footer my_name={my_name} project_name={project_name}></Footer>  
    </div>
   );
 }
