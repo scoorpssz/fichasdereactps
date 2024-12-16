50881bd43b3377be8896fc388544a88e068f17df
José Cláudio <jvalentino9.2005@gmail.com>
2024-12-16 11:13:01 +0000

    1herois

--- src/app/Components/Content.tsx
+++ src/app/Components/Content.tsx
@@ -2,6 +2,12 @@ const Content = () =>{
     return(
         <div>
             <h1>Top-3 Herois</h1>
+        <div>
+        <table>
+            <img src="https://i.pinimg.com/474x/91/8d/e3/918de335d4e6b509932d3bef3e51c92d.jpg" alt="Deadpool"></img>
+        </table>
+        
+        </div>
         </div>
     )
 }
