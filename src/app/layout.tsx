4971ecccc75df6043f3f8e925906783c0b2b8b8a
José Cláudio <jvalentino9.2005@gmail.com>
2024-12-16 17:23:24 +0000

    falta ficha 5 a 7

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-a
+
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
