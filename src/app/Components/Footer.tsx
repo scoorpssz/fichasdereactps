21cf30da860f676912589637771871f0004734b8
José Cláudio <jvalentino9.2005@gmail.com>
2024-12-16 09:17:42 +0000

    a

---
+++ src/app/Components/Footer.tsx
@@ -0,0 +1,9 @@
+const Footer = () => {
+    return(
+        <div>
+          <h1>League of Heroes, desenvolvido por José Cláudio</h1>  
+        </div>  
+    )
+}
+
+export default Footer;
---
+++ src/app/Components/Header.tsx
@@ -0,0 +1,10 @@
+const Header = () => {
+    return(
+        <div>
+        <h1>League of Heroes</h1>
+        <h3>Desenvolvido por José Cláudio</h3>
+        </div>
+    )
+}
+
+export default Header;
