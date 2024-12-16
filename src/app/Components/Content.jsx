84a215426142b4fcfeef45bf7965359092d3ba26
José Cláudio <jvalentino9.2005@gmail.com>
2024-12-16 17:21:43 +0000

    falta a ficha 5 a 7

--- src/app/Components/Content.jsx
+++ src/app/Components/Content.jsx
@@ -1,10 +1,20 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import HeroInfo from './HeroInfo';
 import heroesList from '../shared/heroeslist';
+import Loader from './Loader'; 
 
 const Content = () => {
   const [listOfHeroes, setListOfHeroes] = useState(heroesList); 
   const [favoriteHeroes, setFavoriteHeroes] = useState([1, 5, 6]);
+  const [loading, setLoading] = useState(true); 
+
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      setLoading(false); 
+    }, 3000);
+
+    return () => clearTimeout(timeout); 
+  }, []);
 
   const generateRandomFavorites = () => {
     let randomFavorites = [];
@@ -22,6 +32,8 @@ const Content = () => {
 
   return (
     <div className="content">
+      {loading && <Loader />} 
+      
       <button onClick={generateRandomFavorites}>Mudar Favoritos</button>
       <div className="heroes-grid">
         {listOfHeroes
