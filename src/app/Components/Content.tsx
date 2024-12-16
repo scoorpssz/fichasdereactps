e27f7b9f647bb201e9cb810f6ace907f9521a93c
José Cláudio <jvalentino9.2005@gmail.com>
2024-12-16 11:28:49 +0000

    falat2

--- src/app/Components/Content.tsx
+++ src/app/Components/Content.tsx
@@ -3,10 +3,22 @@ const Content = () =>{
         <div>
             <h1>Top-3 Herois</h1>
         <div>
-        <table>
-            <img src="https://i.pinimg.com/474x/91/8d/e3/918de335d4e6b509932d3bef3e51c92d.jpg" alt="Deadpool"></img>
-        </table>
-        
+            <table>
+                <img src="https://i.pinimg.com/474x/91/8d/e3/918de335d4e6b509932d3bef3e51c92d.jpg" alt="Deadpool" />
+                <h4> Deadpool </h4>
+            </table>
+        </div>
+        <div>
+            <table>
+                <img src="" alt="" />
+                <h4></h4>
+            </table>
+        </div>
+        <div>
+            <table>
+                <img src="" alt="" />
+                <h4></h4>
+            </table>
         </div>
         </div>
     )
