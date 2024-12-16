644bfe6dba8abe518771c5e9d6dee5bc571b6e70
José Cláudio <jvalentino9.2005@gmail.com>
2024-12-16 10:06:22 +0000

    content

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,12 +2,12 @@ import Image from "next/image";
 import styles from "./page.module.css";
 import Header from './Components/Header';
 import Footer from './Components/Footer';
-
+import Content from './Components/Content';
 export default function Home() {
   return (
    <div>
     <Header></Header>
-
+    <Content></Content>
     <Footer></Footer>
    </div>
   );
